b7e7f86c2be9666d83c3c512f7810b9842a51b0a
Marcus <marcus.h.lancaster@gmail.com>
2018-03-17 23:38:34 +0000

    added new test file and commented the BSTBag class and updated WordProcessor2 class to accept filename arguments

--- BSTBag.java
+++ BSTBag.java
@@ -2,25 +2,40 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class BSTBag<E extends Comparable<E>> implements Bag<E> {
-
-	
 	private BSTBag.BSTNode<E> root;
 	
+	/**
+	 * Constructor for Bag, sets root node to null.
+	 */
 	public BSTBag() { root = null; }
 	
 	/**
 	 * Inner Node class
+	 * This class stores an CountedElement and the nodes which are connected
+	 * to this node. 
 	 */
 	private static class BSTNode<E extends Comparable<E>> {
 		protected CountedElement<E> element;
 		protected BSTNode<E> left, right;
 		
+		/**
+		 * When creating a new node, a new counted element s produced,
+		 * which will always start with a count of 1.
+		 * @param elem the element to be store in the node
+		 */
 		protected BSTNode(E elem) {
 			this.element = new CountedElement<E>(elem,1);
 			this.left = null;
 			this.right = null;
 		}
 		
+		/**
+		 * method to remove an node from the BST. If the node has only
+		 * one child, this is returned to the remove method to replace,
+		 * the node being deleted. If there are 2 child nodes it will return
+		 * the left most child of the right hand subtree
+		 * @return the node to replace the topMost node
+		 */
 		public BSTNode<E> deleteTopMost() {
 			if (this.left == null) {
 				return this.right;
@@ -33,6 +48,12 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 			}
 		}
 		
+		/**
+		 * Method to find the left most node of a subtree.
+		 * Keeps going to the left child until the left child
+		 * is null
+		 * @return the left most node in the subtree
+		 */
 		private CountedElement<E> getLeftMost() {
 			BSTNode<E> curr = this;
 			while (curr.left!=null) {
@@ -41,16 +62,15 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 			return curr.element;
 		}
 		
+		/**
+		 * Method to remove the left most node of a subtree and replace
+		 * the root of the subtree with the child of the new leftmost element.
+		 * @return the leftmost 
+		 */
 		private BSTNode<E> deleteLeftMost() {
 			if (this.left == null) {
 				return this.right;
 			} else {
-				BSTNode<E> parent = this;
-				BSTNode<E> curr = this.left;
-				while (curr.left != null) {
-					parent = curr;
-					curr = curr.left;
-				}
 				return this;
 			}
 		}
@@ -58,10 +78,17 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 		
 	/**
 	 * Inner Iterator class
+	 * returns an iterator objects that implements the methods required for an
+	 * iterator class
 	 */
 	private class IteratorObject implements Iterator<E> {
 		private Stack<BSTBag.BSTNode<E>> track;
 		
+		/**
+		 * Constructor for IteratorObject. creates a trace of all nodes in the 
+		 * bag and stores the left most nodes of the BST such that the right sub-trees
+		 * can be iterated through
+		 */
 		private IteratorObject() {
 			track = new LinkedStack<BSTBag.BSTNode<E>>();
 			for (BSTBag.BSTNode<E> curr = root ; curr != null ; curr = curr.left) {
@@ -69,28 +96,44 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 			}
 		}
 		
+		/**
+		 * Method to test if there are any more nodes that have not been traversed
+		 */
 		@Override
 		public boolean hasNext() {
 			return (!track.empty());
 		}
 
+		/**
+		 * Method to return the next element in the iterator.
+		 * Returns the leftmost node from the BST and also adds all the leftmost
+		 * nodes from the right hand subtree of the element being returned.
+		 */
 		@Override
 		public E next() {
 			if (track.empty()) {
 				throw new NoSuchElementException();
 			} else {
+				// remove leftmost node
 				BSTBag.BSTNode<E> place = track.pop();
 				for (BSTBag.BSTNode<E> curr = place.right ; curr != null ; curr = curr.left) {
+					// add all leftmost nodes from right sub-tree
 					track.push(curr);
 				}
+				
+				// Return the element from the BST, not the element and it's count value
 				return place.element.getElement();
 			}
 		}
 		
 	}
 	
+	/**
+	 * Method to validate whether or not there are any values stored in the BST
+	 */
 	@Override
 	public boolean isEmpty() {
+		// if the root is null, there is no data in the tree
 		return (root == null ? true : false);
 	}
 
@@ -114,11 +157,15 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 		}
 	}
 
+	/**
+	 * Method to find the occurrence of an element and return true if it 
+	 * occurs in the BST
+	 */
 	@Override
 	public boolean contains(E element) {
 		BSTBag.BSTNode<E> curr = root;
 		while(curr != null) {
-			if (curr.element.toString().equals(element.toString())) {
+			if (element.compareTo(curr.element.getElement()) == 0) {
 				return true;
 			} else if (element.compareTo(curr.element.getElement()) > 0) {
 				curr = curr.right;
@@ -129,6 +176,13 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 		return false;
 	}
 
+	/**
+	 * Method to test of this instance of a binary search tree is the same as
+	 * another BST - regardless of the ordering of nodes.
+	 * 
+	 * Iterates through all elements of other BST and tests if this instance
+	 * contains an element with the same value
+	 */
 	@Override
 	public boolean equals(Bag<E> that) {
 		Iterator<E> it = that.iterator();
@@ -140,36 +194,54 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 		return true;
 	}
 
+	/**
+	 * method to delete all elements in the BST by removing the root node
+	 * (making all other nodes inaccessible)
+	 */
 	@Override
 	public void clear() {
 		root = null;
 	}
 
+	/**
+	 * Method to add a node to the BST. Searches the BST for the location it belongs to
+	 * and then if that element already exists it increments it's count (assuming it
+	 * is a CountedElement), otherwise it creates a new node in the location and creates a
+	 * link.
+	 */
 	@Override
 	public void add(E element) {
 		BSTBag.BSTNode<E> curr = root;
+		// if the root node is null, this element is now the root node.
 		if (curr == null) {
 			System.out.println("First node is: "+element.toString());
 			root = new BSTBag.BSTNode<E>(element);
 			return;
 		}
+		
+		// Find the location the node belongs in
 		while (true) {
 			BSTBag.BSTNode<E> parent = curr;
 			if(element.compareTo(curr.element.getElement()) == 0) {
+				// If the element already exists increment the count value and terminate
 				System.out.println(element.toString()+" is already in the tree - incrementing");
 				curr.element.setCount(curr.element.getCount()+1);
 				return;
 			} else if (element.compareTo(curr.element.getElement()) > 0) {
+				// if the new node is bigger than the current node, move right down the tree
 				System.out.println(element.toString()+" is right of "+curr.element.toString());
 				 curr = curr.right;
 				 if (curr == null) {
+					 // if there is no value to the right, create a new node here.
 					 parent.right = new BSTBag.BSTNode<E>(element);
 					 return;
 				 }
 			} else {
+				// If the element is smaller than the current node, move left down the tree.
 				System.out.println(element.toString()+" is left of "+curr.element.toString());
 				curr = curr.left;
 				if (curr == null) {
+					// if there is no node here, create a new node
 					parent.left = new BSTBag.BSTNode<E>(element);
 					return;
 				}
@@ -177,46 +249,56 @@ public class BSTBag<E extends Comparable<E>> implements Bag<E> {
 		}
 	}
 
+	/**
+	 * Method to remove the specified node from the BST if it exists in the BST.
+	 */
 	@Override
 	public void remove(E element) {
 		BSTBag.BSTNode<E> parent, curr;
 		parent = null;
 		curr = root;
+		// if the root node is null, terminate
 		if (curr == null) { return; };
-		// 1. find the node containing the value
-		// 2. if node has no children, replace parent left/right node with null
-		// 3. if has 1 child, replace parent left/right node with child node
-		// 4a. if has 2 children, replace parent left/right node with leftmost child of right subtree
-		// 4b. if leftmost child has n 
+		
+		// Search the array for the value to be removed
 		while (true) {
 			if (curr == null) {
+				// if the bottom of the BST has been reached, terminate
 				return;
 			} else if (element.compareTo(curr.element.getElement()) == 0) {
+				// We have found the element to be removed.
 				if (curr.element.getCount() > 1) {
+					// if the element a count of more than 1, do not remove the node, but decrement it's value
 					System.out.println("There are "+curr.element.getCount()+" instances of "+curr.element.getElement()+" - decrementing");
 					curr.element.setCount(curr.element.getCount()-1);
 					return;
 				} else {
+					// if there is only a count of 1. find the replacement node of the deleted node
+					// this is implemented in the Node inner class
 					BSTBag.BSTNode<E> replacement = curr.deleteTopMost();
 					if (curr == root) {
+						// if the current node is the root node, replace the root with its replacement
 						root = replacement;
 					} else if (curr == parent.left) {
+						// if the deleted node is a left child node, update the parent node
 						parent.left = replacement;
 					} else if (curr == parent.right) {
+						// if the deleted node is a right child node, update the parent node
 						parent.right = replacement;
-					} else {
-						// node has no parents :'(
-						root = curr;
 					}
 					return;
 				}
 			} else {
+				// if the node has not been found, traverse the BST until it is found
 				parent = curr;
 				curr = ((element.compareTo(curr.element.getElement()) > 0) ? parent.right : parent.left);
 			}
 		}
 	}
 
+	/**
+	 * Method to return the IteratorObject inner class.
+	 */
 	@Override
 	public Iterator<E> iterator() {
 		return new IteratorObject();
--- WordProcessor2.java
+++ WordProcessor2.java
@@ -1,53 +1,39 @@
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class WordProcessor2 {
-	public static void main(String[] args) throws FileNotFoundException{
-		// TODO Auto-generated method stub
-//		BSTBag<CountedElement<String>> b = new BSTBag<CountedElement<String>>();
-//		System.out.println("Tree is empty? "+b.isEmpty());
-//		b.add(new CountedElement<String>("Marcus"));
-//		b.add(new CountedElement<String>("Is"));
-//		b.add(new CountedElement<String>("A"));
-//		b.add(new CountedElement<String>("Cool"));
-//		b.add(new CountedElement<String>("Dude"));
-//		b.add(new CountedElement<String>("Right"));
-//		System.out.println("Tree is empty? "+b.isEmpty());
-//		
-//		CountedElement<String> test1 = new CountedElement<String>("WORD");
-//		CountedElement<String> test2 = new CountedElement<String>("WORD");
-//		
-//		System.out.println("Testing if two instances of the same word are equal: "+(test1.compareTo(test2)));
-//		System.out.println("Size test should be 6: "+(b.size()));
-//		
-//		System.out.println("Removing \"A\"");
-//		b.remove(new CountedElement<String>("A"));
-//		System.out.println("\"A\" no longer in BST? "+!b.contains(new CountedElement<String>("A")));
-//		System.out.println("Adding \"A\" back in - should go to left of \"Cool\"");
-//		b.add(new CountedElement<String>("A"));
-//		
-//		System.out.println("Removing \"Cool\"");
-//		b.remove(new CountedElement<String>("Cool"));
-//		System.out.println("\"Cool\" no longer in BST? "+!b.contains(new CountedElement<String>("Cool")));
-//		System.out.println("Adding \"Cool\" back in - should go to right of \"A\"");
-//		b.add(new CountedElement<String>("Cool"));
-//		
-//		Iterator<CountedElement<String>> it = b.iterator();
-//		
-//		while (it.hasNext()) {
-//			System.out.println(it.next().toString());
-//		}
-//		
-//		BSTBag<CountedElement<String>> a = new BSTBag<CountedElement<String>>();
-//		System.out.println("Tree is empty? "+a.isEmpty());
-//		a.add(new CountedElement<String>("Marcus"));
-//		a.add(new CountedElement<String>("Is"));
-//		a.add(new CountedElement<String>("A"));
-//		a.add(new CountedElement<String>("Cool"));
-//		a.add(new CountedElement<String>("Dude"));
-//		a.add(new CountedElement<String>("Right"));
-//		
-//		System.out.print("Testing if two bags are equal (with different ordering):"+a.equals(b));
+	public static void main(String[] args) throws FileNotFoundException {
+		BSTBag<String> b = new BSTBag<String>();
+		System.out.println("Tree is empty? "+b.isEmpty());
+		b.add("Marcus");
+		b.add("Is");
+		b.add("A");
+		b.add("Cool");
+		b.add("Dude");
+		b.add("Right");
+		System.out.println("Tree is empty? "+b.isEmpty());
+		System.out.println("Size test should be 6: "+(b.size()));
+		
+		System.out.println("Removing \"A\"");
+		b.remove("A");
+		System.out.println("\"A\" no longer in BST? "+!b.contains("A"));
+		System.out.println("Adding \"A\" back in - should go to left of \"Cool\"");
+		b.add("A");
+		
+		System.out.println("Removing \"Cool\"");
+		b.remove("Cool");
+		System.out.println("\"Cool\" no longer in BST? "+!b.contains("Cool"));
+		System.out.println("Adding \"Cool\" back in - should go to right of \"A\"");
+		b.add("Cool");
+		
+		Iterator<String> it = b.iterator();
+		
+		while (it.hasNext()) {
+			System.out.println(it.next().toString());
+		}
+		
 		BSTBag<String> a = new BSTBag<String>();
 		System.out.println("Tree is empty? "+a.isEmpty());
 		a.add("Marcus");
@@ -56,14 +42,37 @@ public class WordProcessor2 {
 		a.add("Cool");
 		a.add("Dude");
 		a.add("Right");
-		a.add("Marcus");
-		a.remove("Marcus");
-		Iterator<String> it = a.iterator();
 		
-		while (it.hasNext()) {
-			System.out.println(it.next());
+		System.out.print("Testing if two bags are equal (with different ordering):"+a.equals(b));
+		BSTBag<String> c = new BSTBag<String>();
+		System.out.println("Tree is empty? "+c.isEmpty());
+		c.add("Marcus");
+		c.add("Is");
+		c.add("A");
+		c.add("Cool");
+		c.add("Dude");
+		c.add("Right");
+		c.add("Marcus");
+		c.remove("Marcus");
+		Iterator<String> iter = c.iterator();
+		
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
 		}
 		
+		BSTBag<String> bag = new BSTBag<String>();
 		
+		for (String filename : args) {
+			// Read in each file
+			FileReader reader = new FileReader(filename);
+			Scanner in = new Scanner(reader);
+
+			// for each word in file
+			while (in.hasNext()) {
+				bag.add(in.next());
+			}
+			
+			in.close();
+		}
 	}
 }
